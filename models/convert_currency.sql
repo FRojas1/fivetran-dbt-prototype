4ae010feb5c20a10ae224dae7570ae0993a6551b
Felipe <1looploop1@gmail.com>
2026-03-23 11:05:42 +0100

    Update convert_currency.sql

--- models/convert_currency.sql
+++ models/convert_currency.sql
@@ -1,2 +1,5 @@
-SELECT order_id, amount_usd, (amount_usd * 0.91) AS amount_eur 
-FROM rawsales.google_sheets.raw_sales_data
+SELECT 
+    order_id, 
+    amount_usd, 
+    (amount_usd * 0.91) AS amount_eur 
+FROM google_sheets.rawsales
